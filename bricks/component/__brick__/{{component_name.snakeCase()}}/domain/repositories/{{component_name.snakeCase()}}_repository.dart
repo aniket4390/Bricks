32d48a5ead21a30c0638e0d2b6531e2c2ce7bbfc
Aniket Ambre <39184305+aniket4390@users.noreply.github.com>
2025-07-24 15:06:10 +0530

    Update {{component_name.snakeCase()}}_repository.dart

--- bricks/component/__brick__/{{component_name.snakeCase()}}/domain/repositories/{{component_name.snakeCase()}}_repository.dart
+++ bricks/component/__brick__/{{component_name.snakeCase()}}/domain/repositories/{{component_name.snakeCase()}}_repository.dart
@@ -1,5 +1,5 @@
 import 'package:dartz/dartz.dart';
-import 'package:jiogate_2/app/core/errors/failures.dart';
+import 'package:project_name/app/core/errors/failures.dart';
 //import
 
 abstract class {{component_name.pascalCase()}}Repository {
